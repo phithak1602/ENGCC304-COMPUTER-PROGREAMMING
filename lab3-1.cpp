1b5f414eef316f20fb3c2f916ebe9335ce55abdb
Phithak Boonnoi <gtaninphitak@gmail.com>
2022-07-18 20:07:07 +0700

    lab3 codex

---
+++ lab3-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main (){
+    int num1 ;
+    float num2 ;
+    char abc ;
+    printf("Please Enter value :") ;
+    scanf("%d %f %c",&num1,&num2,&abc) ;
+    printf("%d %.2f %c",num1,num2,abc) ;
+    return 0 ;
+}
