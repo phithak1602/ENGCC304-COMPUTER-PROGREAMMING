76a743c6ee53a29836ac7ededb9f95e4d8557acf
Phithak Boonnoi <gtaninphitak@gmail.com>
2022-07-26 21:23:48 +0700

    lab5 if else switch case

---
+++ lab5-1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int years ;
+    printf( "Input years : " ) ;
+    scanf( "%d", &years ) ;
+    years = years % 4 ;
+    
+    if ( years == 0 ) {
+        printf( "Feb = 29 days" ) ;
+    } else if ( years != 0 && years <= 3 ) {
+        printf( "Feb = 28 days" ) ;
+    } else {
+        printf( "Error, Please try again." ) ;
+    } //end if else
+    
+    return 0 ;
+} //end funtion
---
+++ lab5-2.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int years ;
+    printf( "Input years : " ) ;
+    scanf( "%d", &years ) ;
+    years = years % 4 ;
+    
+    switch (years) {
+        case 0 :
+            printf( "Feb = 29 days" ) ;
+        break ;
+        case 1 :
+            printf( "Feb = 28 days" ) ;
+        break ;
+        case 2 :
+            printf( "Feb = 28 days" ) ;
+        break ;
+        case 3 :
+            printf( "Feb = 28 days" ) ;
+        break ;
+        default:
+            printf( "Error, Please try again." ) ;
+        break ;
+   } //end switch case
+    
+    return 0 ;
+} //end funtion
